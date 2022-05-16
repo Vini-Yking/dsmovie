e5b11b69bb3c835e1a7ec543aa528f179b8552cf
Vini-Yking <mvdsfg@gmail.com>
2022-05-16 00:33:45 -0300

    show Score

--- front/src/components/MovieCard/index.tsx
+++ front/src/components/MovieCard/index.tsx
@@ -1,26 +1,28 @@
 import MovieScore from "components/MovieScore";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
 type Props = {
-    movie: Movie;
-}
+  movie: Movie;
+};
 
-function MovieCard( {movie} : Props){
-    return (
-<div>
-    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-    <div className="dsmovie-card-bottom-container">
+function MovieCard({ movie }: Props) {
+  return (
+    <div>
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
-     
-     
-        <Link to={`/form/${movie.id}`}>
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-        </Link>
-        
-    </div>
-</div>
+        <MovieScore count={movie.count} score={movie.score} />
 
-    );
-} export default MovieCard;
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default MovieCard;
